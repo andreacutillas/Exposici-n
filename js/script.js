eaa911bb963509d71a5326df2fb317904ed471dc
Vicky <vikimrtz@gmail.com>
2020-05-29 18:24:43 +0200

    intento primero de responsive

--- js/script.js
+++ js/script.js
@@ -96,3 +96,83 @@ window.onscroll = function () {
                 </ul>
             </nav>  
         </nav>
+*/      
+        
+        
+        
+function openMenu() {
+    "use strict";
+    //Buscamos los li y los guardamos en una variable
+    var opciones = document.getElementById("menu").firstElementChild.children;
+    //La variable i nos ayudará a recorrer el bucle e ir presentando las opciones
+    var i;
+    for (i = 0; i < opciones.length; i = i + 1) {
+        opciones[i].style.display = "block";
+    }
+}
+
+function closeMenu() {
+    "use strict";
+    //Solo cerramos el menú cuando la pantalla es pequeña (menor a 700px de ancho)
+    if (window.innerWidth <= 700) {
+        var opciones = document.getElementById("menu").firstElementChild.children;
+        var i;
+        opciones[0].style.display = "block";
+        for (i = 1; i < opciones.length; i = i + 1) {
+            opciones[i].style.display = "none";
+        }
+    }
+}
+
+function menu() {
+    "use strict";
+    if (document.getElementById("menu").firstElementChild.lastElementChild.style.display === "block") {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+}
+
+function openedMenu() {
+    "use strict";
+    var opciones = document.getElementById("menu").firstElementChild.children;
+    var i;
+    opciones[0].style.display = "none";
+    for (i = 1; i < opciones.length; i = i + 1) {
+        opciones[i].style.display = "block";
+    }
+}
+
+
+window.onresize = function () {
+    "use strict";
+    if (window.innerWidth <= 700) {
+        closeMenu();
+    } else {
+        openedMenu();
+    }
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
